d77e4147862afe41e87e181111b591997ecaea72
moonlight <362652565@qq.com>
2018-01-30 00:34:37 +0800

    重新部署

--- dist/animation-resume.js
+++ dist/animation-resume.js
@@ -69,7 +69,7 @@ require = (function (modules, cache, entry) {
 
   // Override the current require with this new one
   return newRequire;
-})({7:[function(require,module,exports) {
+})({8:[function(require,module,exports) {
 var bundleURL = null;
 function getBundleURLCached() {
   if (!bundleURL) {
@@ -100,7 +100,7 @@ function getBaseURL(url) {
 exports.getBundleURL = getBundleURLCached;
 exports.getBaseURL = getBaseURL;
 
-},{}],4:[function(require,module,exports) {
+},{}],7:[function(require,module,exports) {
 var bundle = require('./bundle-url');
 
 function updateLink(link) {
@@ -132,13 +132,13 @@ function reloadCSS() {
 
 module.exports = reloadCSS;
 
-},{"./bundle-url":7}],3:[function(require,module,exports) {
+},{"./bundle-url":8}],3:[function(require,module,exports) {
 
         var reloadCSS = require('_css_loader');
         module.hot.dispose(reloadCSS);
         module.hot.accept(reloadCSS);
       
-},{"_css_loader":4}],5:[function(require,module,exports) {
+},{"_css_loader":7}],5:[function(require,module,exports) {
 var global = (1,eval)("this");
 
 /* **********************************************
@@ -2516,7 +2516,7 @@ writeCss("", css1, 1, () => {
     });
   });
 });
-},{"./css/default.css":3,"prismjs":5,"marked":6}],12:[function(require,module,exports) {
+},{"./css/default.css":3,"prismjs":5,"marked":6}],9:[function(require,module,exports) {
 
 var global = (1, eval)('this');
 var OldModule = module.bundle.Module;
@@ -2536,7 +2536,7 @@ module.bundle.Module = Module;
 
 if (!module.bundle.parent && typeof WebSocket !== 'undefined') {
   var hostname = '' || location.hostname;
-  var ws = new WebSocket('ws://' + hostname + ':' + '53371' + '/');
+  var ws = new WebSocket('wss://' + hostname + ':' + '65336' + '/');
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
 
@@ -2637,5 +2637,5 @@ function hmrAccept(bundle, id) {
     return hmrAccept(global.require, id);
   });
 }
-},{}]},{},[12,2])
+},{}]},{},[9,2])
 //# sourceMappingURL=/dist/animation-resume.map
